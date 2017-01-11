54bbb0cfe1fd3bbb8689dd71093d5a0909d3a70c
ECostanzo <mail@ecostanzo.de>
2017-01-11 23:24:26 +0100

    Solldatenaktualisierung

--- batsim.py
+++ batsim.py
@@ -1,23 +1,23 @@
-# Fahrrad Controller Simulator
+# Batterie Controller Simulator
 #
-# Sendet Testdaten im gleichen Takt wie der Fahrrad Controller an den Server
+# Sendet Testdaten im gleichen Takt wie der Batterie Controller an den Server
 
 import socket, time, random, string, os
 
-SERVER = "127.0.0.1"
+SERVER = "192.168.1.6"
 PORT = 90
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 nPackets = 0
 
 def generateData():
-	pGen 	= os.urandom(1) #b"\x3A"
-	T 		= os.urandom(2) #b"\x3A\x98"
-	nLm 	= os.urandom(2) #b"\x00\xFF"
+	pGen = os.urandom(1) #b"\x3A"
+	T = os.urandom(2)    #b"\x3A\x98"
+	nLm = os.urandom(2)  #b"\x00\x05"
 	
 	return b"%s%s%s" % (pGen, T, nLm)
 
 if __name__ == "__main__":
 	while 1:
-		time.sleep(5)
+		time.sleep(0.5)
 		if(sock.sendto(generateData(), (SERVER, PORT))): nPackets += 1
 		print("Sending Packet #", nPackets)
--- server.py
+++ server.py
@@ -2,65 +2,122 @@
 #
 # Empfängt Daten vom Fahrrad Controller und kommuniziert mit der API
 
-import socket, requests, random, os
+import socket, requests, random, os, json
 clear = lambda: os.system('cls')
 
 API_URL = "http://localhost/fahrrad/public/"
 
-# Paketformat:
-# 8 Byte
-# 	Frame-No 					(1 Byte)
-#	Power W 					(1 Byte)
-# 	T: Zeit für N umdrehungen 	(2 Byte)
-#	N: Anzahl Umdrehungen 		(1 Byte)
-#	Pedalzeit halber Umlauf 	(2 Byte)
-#	Mode 						(1 Byte)
+class Fahrrad:
+	def __init__(self, ip, mac):
+		self.ip = ip
+		self.mac = mac
+		
+		self.strecke = 0
+		self.geschwindigkeit = 0
+		self.istLeistung = 0
+		
+		self.sollDrehmoment = None # 1 Byte
+		self.sollLeistung   = None # 1 Byte
+		
+		self.reifenUmfang = 2.1
+		self.u = 10
+		
+		self.data 	= 0
+		self.pGen 	= 0
+		self.T 		= 0
+		self.nLm 	= 0
 
-def computeData(data):
-	reifenUmfang = 2.1													# M
-	u = 10
+	def sollDatenAktualisieren(self):
+		response = requests.get(API_URL + "data/" + self.ip)
+		if(response.status_code == 200):
+			json_data = json.loads(response.text)
+			
+			sollDrehmoment = json_data["fahrrad"]["sollDrehmoment"] # Int
+			sollLeistung   = json_data["fahrrad"]["sollLeistung"] 	# Int
+			
+			# Ist kein Wert, oder beide Werte gesetzt liegt ein Fehler vor und keine Änderung wird angewendet
+			if((sollLeistung == None and sollDrehmoment == None) or (sollLeistung != None and sollDrehmoment != None)):
+				print("Keine Änderung, da fehlerhafte Daten")
+			else:
+				# Leistung anpassen
+				if(sollLeistung != None and sollDrehmoment == None):
+					if(self.sollLeistung != bytes([sollLeistung])): # Änderung notwendig?
+						self.sollLeistung = bytes([sollLeistung])
+						self.sendToFC(1)
+					
+				# Drehmoment anpassen
+				if(sollLeistung == None and sollDrehmoment != None):
+					if(self.sollDrehmoment != bytes([sollDrehmoment])): # Änderung notwendig?
+						self.sollDrehmoment = bytes([sollDrehmoment])
+						self.sendToFC(2)
+	
+	# Sendet die Daten an den Fahrradcontroller zur Anpassung der Regelung
+	def sendToFC(self, mode):
+		bytestring = bytes([mode]) + (self.sollLeistung if mode == 1 else self.sollDrehmoment)
+		print(bytestring)
+		
+	def compute(self, data):
+		# Paketformat (FC-Daten):
+		# 8 Byte
+		# 	Frame-No 					(1 Byte)
+		#	Power W 					(1 Byte)
+		# 	T: Zeit für N umdrehungen 	(2 Byte)
+		#	N: Anzahl Umdrehungen 		(1 Byte)
+		#	Pedalzeit halber Umlauf 	(2 Byte)
+		#	Mode 						(1 Byte)
+		self.data = data
 
-	pGen = data[0] 														# mittlere IST-Leistung in W
-	T = int.from_bytes([data[1],data[2]], byteorder='big')				# Zeit für eine Tretlagerumdrehung in Sekunden
-	nLm = int.from_bytes([data[3],data[4]], byteorder='big')			# Umdrehungen Lichtmaschine
-	
-	strecke = nLm * (reifenUmfang / u)
-	geschwindigkeit = (strecke / (T / 15625)) * 3.6
-	istLeistung = pGen
-	
-	clear()
-	print("Raw: ", data)
-	print()
-	print("pGen: ", pGen)
-	print("T: ", T)
-	print("nLm: ", nLm)
-	print()
-	print("Umdrehungen: ", nLm)
-	print("Zeit in Ticks: ", T)
-	print("Zeit in Sekunden: ", T / 15625)
-	print("Strecke: ", strecke)
-	print("Geschwindigkeit: ", geschwindigkeit)
-	print("istLeistung: ", istLeistung)
-	
-	return strecke, geschwindigkeit, istLeistung
+		self.pGen 	= data[0] 											 # mittlere IST-Leistung in W
+		self.T 		= int.from_bytes([data[1],data[2]], byteorder='big') # Zeit für eine Tretlagerumdrehung in Sekunden
+		self.nLm 	= int.from_bytes([data[3],data[4]], byteorder='big') # Umdrehungen Lichtmaschine
+		
+		self.strecke 		 = self.nLm * (self.reifenUmfang / self.u)
+		self.geschwindigkeit = (self.strecke / (self.T / 15625)) * 3.6
+		self.istLeistung 	 = self.pGen
+		
+		# Änderungen an die API senden
+		requests.post(API_URL + "data", data = { 
+			"ip": self.ip, 
+			"strecke": self.strecke,
+			"geschwindigkeit": self.geschwindigkeit,
+			"istLeistung": self.istLeistung
+		})
+		
+	def printData(self):
+		print("Raw: ", self.data)
+		print()
+		print("pGen: ", self.pGen)
+		print("T: ", self.T)
+		print("nLm: ", self.nLm)
+		print()
+		print("Umdrehungen: ", self.nLm)
+		print("Zeit in Ticks: ", self.T)
+		print("Zeit in Sekunden: ", self.T / 15625)
+		print("Strecke: ", self.strecke)
+		print("Geschwindigkeit: ", self.geschwindigkeit)
+		print("istLeistung: ", self.istLeistung)
 
 if __name__ == "__main__":
+	fahrraeder = [
+		Fahrrad("10.0.0.0", "00:00:00:00:00:00"),
+		Fahrrad("10.0.0.1", "00:00:00:00:00:01"),
+		Fahrrad("10.0.0.2", "00:00:00:00:00:02")
+	]
+	
 	socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	socket.bind(("", 90))
 
-	print("waiting on port:", 90)
+	print("Waiting on port:", 90)
 
 	while 1:
-		try:
-			data, addr = socket.recvfrom(8)
-			print(addr, data)
-			strecke, geschwindigkeit, istLeistung = computeData(data)
-			
-			requests.post(API_URL + "data", data = {
-				"ip": "10.0.0."+ str(random.choice([1,2,3])),
-				"strecke": strecke,
-				"geschwindigkeit": geschwindigkeit,
-				"istLeistung": istLeistung
-			})
-		except:
-			print("Error")
+		# Ist Daten aktualisieren
+		data, addr = socket.recvfrom(8)
+		
+		#clear()
+		for fahrrad in fahrraeder:
+			#if(fahrrad.ip == addr): # Daten kommen von diesem Fahrrad
+			fahrrad.compute(data)
+				
+		# Solldaten auswerten
+		for fahrrad in fahrraeder:
+			fahrrad.sollDatenAktualisieren()
